1e21843f8daf9782d51a63a4e32078afe33547f1
RaduAlbastroiu <albastroiuradug@gmail.com>
2018-11-23 18:15:17 +0200

    Initial Commit

---
+++ CityCare/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CityCare
+//
+//  Created by Radu Albastroiu on 23/11/2018.
+//  Copyright © 2018 Radu Albastroiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
