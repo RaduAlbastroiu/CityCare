8c2a6a0608bd4c6dd4a8a2c47d34edb7095f657f
anisiai <anisiai@yahoo.com>
2018-11-25 06:26:53 +0200

    Add comment view + request done

--- CityCare/NetworkManager/NetworkManager.swift
+++ CityCare/NetworkManager/NetworkManager.swift
@@ -345,11 +345,19 @@ class NetworkManager: NSObject {
         
         let json = commentModel.toJson(issueId: issueId)
         request.httpBody = json.data(using: .utf8)
-
+print(json)
         let task = URLSession.shared.dataTask(with: request) { data, response, error in
             guard let data = data, let httpResponse = response as? HTTPURLResponse else {
                 return
             }
+            
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(response)")
+            }
+            
+            let responseString = String(data: data, encoding: .utf8)
+            print("responseString = \(responseString)")
 
             let status = httpResponse.statusCode
             if(status == 200) {
